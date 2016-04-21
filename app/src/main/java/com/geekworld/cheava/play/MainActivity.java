e8775cabb3b4d2d829e9f473946314f73f4517d1
Cheava <hemixianyuan@gmail.com>
2016-04-21 16:45:23 +0800

    Toast Lesson

--- app/src/main/java/com/geekworld/cheava/play/MainActivity.java
+++ app/src/main/java/com/geekworld/cheava/play/MainActivity.java
@@ -6,7 +6,10 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView01 = (TextView) findViewById(R.id.myTextView01);
         Resources resources = getBaseContext().getResources();
-        Drawable HippoDrawable = resources.getDrawable(R.drawable.abc_item_background_holo_dark);
+        Drawable HippoDrawable = resources.getDrawable(R.drawable.abc_list_selector_disabled_holo_light);
         mTextView01.setBackgroundDrawable(HippoDrawable);
 
         mTextView02 = (TextView) findViewById(R.id.textView2);
         CharSequence str_2 = getString(R.string.textView2);
         String str_3 = ", Second hello !";
         mTextView02.setText(str_2 + str_3  );
+        Button button1 = (Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"It's interesting! ", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
